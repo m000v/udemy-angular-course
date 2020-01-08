d687f22d0785c431071f5d3632e6b6225556bf60
Ilya Morozov <i.morozov@zmeke.com>
2020-01-08 21:26:33 +0300

    lesson 1 answer

--- basics-assignment-1-start/src/app/app.module.ts
+++ basics-assignment-1-start/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
+import { AlertComponent } from './alert/alert.component';
+import { WarningAlertComponent } from './warningalert/warningalert.component';
+import { SuccessAlertComponent } from './successalert/successalert.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AlertComponent,
+    WarningAlertComponent,
+    SuccessAlertComponent
   ],
   imports: [
     BrowserModule,
